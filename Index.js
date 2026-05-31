eee0d008f04521d57b49676a9d1de1472d63b8cd
BHARATH <bharath978930@gmail.com>
2026-05-31 07:43:05 +0530

    Changed

--- Index.js
+++ Index.js
@@ -3,6 +3,7 @@ const scriptURL = 'https://script.google.com/macros/s/AKfycbwRWWyjPLw3uhbSG0uggR
   const msg = document.getElementById("msg")
   const err = document.getElementById("err")
 
+  
   form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
